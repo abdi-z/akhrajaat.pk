c02cffa82ed233eb81320b53247b804fec5d5eb4
Abdul Rehman Aziz <fa19-bcs-016@cuilahore.edu.pk>
2022-05-16 22:43:41 +0500

    ..

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -53,7 +53,7 @@ export const useFirestore = (collection) => {
 
     try {
       const createdAt = timestamp.fromDate(new Date());
-      const addedDocument = await ref.add(doc);
+      const addedDocument = await ref.add({ ...doc, createdAt });
       dispatchIfNotCancelled({
         type: "ADDED_DOCUMENT",
         payload: addedDocument,
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,18 +1,16 @@
-import styles from './Home.module.css'
-import TransactionForm from './TransactionForm'
-import React from 'react'
-import { useAuthContext } from '../../hooks/useAuthContext'
+import styles from "./Home.module.css";
+import TransactionForm from "./TransactionForm";
+import React from "react";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 export default function Home() {
-  const {user}=useAuthContext()
+  const { user } = useAuthContext();
   return (
     <div className={styles.container}>
-      <div className={styles.content}>
-        Transaction List
-      </div>
+      <div className={styles.content}>Transaction List</div>
       <div className={styles.sidebar}>
-        <TransactionForm uid={user.id}/>
+        <TransactionForm uid={user.id} />
       </div>
     </div>
-  )
+  );
 }
