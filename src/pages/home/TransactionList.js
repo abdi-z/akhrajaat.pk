b0b59a322df283a78ed0c3f480ee747a0af3af3f
Abdul Rehman Aziz <fa19-bcs-016@cuilahore.edu.pk>
2022-05-17 13:19:14 +0500

    added some animations, finalized list of transactions, deployed on firebase

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -11,7 +11,8 @@ export default function TransactionList({ transactions }) {
       {transactions.map((transaction) => (
         <li key={transaction.id}>
           <p className={styles.name}>{transaction.name}</p>
-          <p className={styles.amount}>${transaction.amount}</p>
+          <p className={styles.amount}>Rs.{transaction.amount}</p>
+          <p className={styles.date}>{transaction.date}</p>
           <button onClick={() => deleteDocument(transaction.id)}>x</button>
         </li>
       ))}
